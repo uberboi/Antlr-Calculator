83cf85b5f1c8afb70e7d62e58198c247ed3bfbd0
sthuang <sthuang@ucsc.edu>
2018-09-23 01:04:34 -0700

    Calculator visitor class added along with AST printer
    Calculator class added to do computation functions. AST printer added to check correctectness of parse tree. Lots of print functions to check.

--- src/main/java/org/sthuang/AntlrCalculator/App.java
+++ src/main/java/org/sthuang/AntlrCalculator/App.java
@@ -1,13 +1,31 @@
 package org.sthuang.AntlrCalculator;
 
-/**
- * Hello world!
- *
- */
+import org.antlr.v4.runtime.CharStream;
+import org.antlr.v4.runtime.CharStreams;
+import org.antlr.v4.runtime.CommonTokenStream;
+import org.antlr.v4.runtime.TokenStream;
+import org.antlr.v4.runtime.tree.ParseTree;
+
+import org.sthuang.AntlrCalculator.calculatorParser.EquationContext;
+
 public class App 
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        String expression = "(4*2) = x";
+        calculator c = new calculator();
+        calculatorParser parser = c.createParser(expression);
+        
+        AstPrinter ast = new AstPrinter();
+        //ast.print(parser.equation());
+        
+        //EquationContext eq = parser.equation();
+        //ast.print(eq);
+        
+        ParseTree tree = parser.equation();
+        System.out.println(new calculator().visit(tree));
+        //System.out.println(expression);
+        System.out.println(Double.valueOf("2e10") + (Double.valueOf("2e10")));
+        
     }
 }
--- src/main/java/org/sthuang/AntlrCalculator/calculator.java
+++ src/main/java/org/sthuang/AntlrCalculator/calculator.java
@@ -1,5 +1,97 @@
 package org.sthuang.AntlrCalculator;
 
-public class calculator extends calculatorBaseVisitor<Integer>{
+import org.antlr.v4.runtime.CharStream;
+import org.antlr.v4.runtime.CharStreams;
+import org.antlr.v4.runtime.CommonTokenStream;
+import org.antlr.v4.runtime.TokenStream;
+import org.antlr.v4.runtime.tree.ParseTree;
+
+public class calculator<T> extends calculatorBaseVisitor<T>{
+	
+	calculatorParser createParser(String expression) {
+		CharStream stream = CharStreams.fromString(expression);
+		calculatorLexer lexer = new calculatorLexer(stream);
+		TokenStream tokenStream = new CommonTokenStream(lexer);
+		return new calculatorParser(tokenStream);
+		
+	}
+	
+	@Override
+	public T visitEquation(calculatorParser.EquationContext ctx) {
+		System.out.println("visitEquation: " + ctx.getText());
+		return visitChildren(ctx);
+	}
+	
+	@Override
+	public T visitExpression(calculatorParser.ExpressionContext ctx) {
+		System.out.println("visitExpression: " + ctx.getText());
+		return visitChildren(ctx);
+	}
+	
+	@Override
+	public T visitMultiplyingExpression(calculatorParser.MultiplyingExpressionContext ctx) {
+		System.out.println("visitMultiplyingExpression: " + ctx.getText());
+		return visitChildren(ctx);
+	}
+	
+	@Override
+	public T visitPowExpression(calculatorParser.PowExpressionContext ctx) {
+		System.out.println("visitPowExpression: " + ctx.getText());
+		return visitChildren(ctx);
+	}
+	
+	@Override
+	public T visitSignedAtom(calculatorParser.SignedAtomContext ctx) {
+		System.out.println("visitSignedAtom: " + ctx.getText());
+		return visitChildren(ctx);
+	}
+	
+	@Override
+	public T visitAtom(calculatorParser.AtomContext ctx) {
+		if(ctx.scientific() != null) {
+			System.out.println("Scientific Atom: " + ctx.getText());
+		} else if(ctx.variable() != null) {
+			System.out.println("Variable Atom: " + ctx.getText());
+		} else if(ctx.constant() != null) {
+			System.out.println("Constnat Atom: " + ctx.getText());
+		} else {
+			System.out.println("Parethesis Atom: " + ctx.getText());
+		}
+		//System.out.println("visitAtom: " + ctx.getText());
+		return visitChildren(ctx);
+		/*
+		return Double.valueOf(ctx.getText());
+		*/
+	}
+	
+	@Override
+	public T visitConstant(calculatorParser.ConstantContext ctx) {
+		System.out.println("visitConstant: " + ctx.getText());
+		return visitChildren(ctx);
+	}
+	
+	@Override
+	public T visitVariable(calculatorParser.VariableContext ctx) {
+		System.out.println("visitVariable: " + ctx.getText());
+		return visitChildren(ctx);
+	}
+	
+	@Override
+	public T visitFunc(calculatorParser.FuncContext ctx) {
+		System.out.println("visitFunc: " + ctx.getText());
+		return visitChildren(ctx);
+	}
+	
+	@Override
+	public T visitFuncname(calculatorParser.FuncnameContext ctx) {
+		System.out.println("visitFuncname: " + ctx.getText());
+		return visitChildren(ctx);
+	}
+	
+	@Override
+	public T visitRelop(calculatorParser.RelopContext ctx) {
+		System.out.println("visitRelop: " + ctx.getText());
+		return visitChildren(ctx);
+	}
 
 }
